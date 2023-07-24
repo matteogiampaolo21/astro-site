e6a262f906fbbb5ab18f5d6e9945c2de81aebc1b
Matteo Giampaolo <doyouthinkysauros@gmail.com>
2023-07-24 15:42:58 +0700

    carousel is done, navbar will be changed again

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,21 +3,13 @@ import './Navbar.css'
 export const Navbar = () => {
 
     return(
-        <div className=" fixed z-50">
-            
-            <label className="hamburger-menu">
-              <input type="checkbox" />
-            </label>
-            <aside className="sidebar">
-              <nav className="flex flex-col gap-5 text-xl pb-3">
-                <div><a href="/" className="w-full block">Home</a></div>
-                <div><a href="/secPage" className="w-full block">Second Page</a></div>
-                <div><a href="/about" className="w-full block">About</a></div>
-                <div><a href="/contact" className="w-full block">Contact</a></div>
-              </nav>
-            </aside>
-                
-        </div>
-        
+       <nav className='bg-slate-700 text-white py-8'>
+        <ul className='flex flex-row justify-center gap-x-5 text-lg sm:gap-x-8 md:gap-x-12'>
+          <li><a href="/">Home</a></li>
+          <li><a href="/secPage">Page</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+       </nav> 
     )
 }
